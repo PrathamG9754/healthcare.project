c292a56bc0bcd8966c292db5ce50ff10c6fc9a76
PrathamG9754 <128122683+PrathamG9754@users.noreply.github.com>
2024-10-22 10:18:31 +0530

    intial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+//Framework Configuration
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
