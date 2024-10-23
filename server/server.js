c2555019617034fa89ce7e5ccfa50176860f4f7a
PrathamG9754 <128122683+PrathamG9754@users.noreply.github.com>
2024-10-23 13:49:25 +0530

    wed work

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 //Framework Configuration
 const express = require("express");
 const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middleware/errorHandler");
+const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 connectDb();
 const app = express();
@@ -10,3 +13,24 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.set('view')
+
+app.use(errorHandler);
+
+// ERROR handling middleware
+app.use(errorHandler);
+
+
+
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+
+
+// APP CONFIG START
+app.listen(port, () =>{
+    console.log(`Server running in port http://localhost:${port}`);
+});
